6643273078ab59ef1537abcea4210d939112754b
Joscelyn <JoscelynRose@gmail.com>
2020-02-07 00:29:08 -0500

    fixed up hamburger menu

--- JS/index.js
+++ JS/index.js
@@ -1,71 +1,67 @@
-
 class ButtonLink {
-    constructor(buttonElement) {
+  constructor(buttonElement) {
+    this.buttonElement = buttonElement;
 
-      this.buttonElement = buttonElement;
+    this.buttonData = this.buttonElement.dataset.job;
 
-  
-      this.buttonData = this.buttonElement.dataset.job;
-
-
-
-    
-      if (this.buttonData === 'all') {
-        this.people = document.querySelectorAll('.people');
-
-      } else {
-        this.people = document.querySelectorAll(`.people[data-job="${this.buttonData}"]`);
-
-      }
-      
-    
-      this.people = Array.from(this.people).map(person => {
-          return new Person(person)});
-      
-      this.buttonElement.addEventListener('click', () => {
-        this.selectPeople();
-      });
+    if (this.buttonData === "all") {
+      this.people = document.querySelectorAll(".people");
+    } else {
+      this.people = document.querySelectorAll(
+        `.people[data-job="${this.buttonData}"]`
+      );
     }
-  
-    selectPeople() {
-  
-      const buttons = document.querySelectorAll('.job-button');
-  
-      buttons.forEach( button => {
-        button.classList.remove('active-element');
-      })
 
-      const people = document.querySelectorAll('.people');
-      people.forEach( person => {
-        person.style.display = 'none';
-      })
-      
-  
-      this.buttonElement.classList.add('active-element');
-  
-      this.people.forEach(person => person.selectPerson());
-    }
+    this.people = Array.from(this.people).map(person => {
+      return new Person(person);
+    });
+
+    this.buttonElement.addEventListener("click", () => {
+      this.selectPeople();
+    });
   }
-  
-  class Person {
-    constructor(personElement){
-      this.personElement = personElement;
-    }
-    selectPerson(){
-      this.personElement.style.display = 'flex';
-    }
-  
+
+  selectPeople() {
+    const buttons = document.querySelectorAll(".job-button");
+
+    buttons.forEach(button => {
+      button.classList.remove("active-element");
+    });
+
+    const people = document.querySelectorAll(".people");
+    people.forEach(person => {
+      person.style.display = "none";
+    });
+
+    this.buttonElement.classList.add("active-element");
+
+    this.people.forEach(person => person.selectPerson());
   }
-  
-let buttons = document.querySelectorAll('.job-button').forEach(button => new ButtonLink(button));
+}
 
-let dropdown = document.querySelector('.dropdown-menu');
+class Person {
+  constructor(personElement) {
+    this.personElement = personElement;
+  }
+  selectPerson() {
+    this.personElement.style.display = "flex";
+  }
+}
 
+let buttons = document
+  .querySelectorAll(".job-button")
+  .forEach(button => new ButtonLink(button));
 
-dropdown.addEventListener('click', function() {
-  let content = document.querySelector('.dropdown-content');
-  content.classList.toggle('display-dropdown');
+let dropdown = document.querySelector(".dropdown-menu");
 
-})
+dropdown.addEventListener("click", function() {
+  let content = document.querySelector(".dropdown-content");
+  content.classList.toggle("display-dropdown");
+});
 
+let close = document.querySelector("#close");
 
+close.addEventListener("click", function() {
+  let content = document.querySelector(".dropdown-content");
+  content.classList.toggle("display-dropdown");
+});
